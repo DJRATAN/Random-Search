16966338fc9882a9c5c7ae3278ba37860c6ec6ee
Ratan Prajapati <ratanprajapati1242@gmail.com>
2025-12-25 07:25:45 +0530

    2nd versin

---
+++ status.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+  chrome.storage.local.get(["browserStartTime","searchCount"], (data) => {
+    if (data.browserStartTime) {
+      const seconds = Math.floor((Date.now() - data.browserStartTime)/1000);
+      document.getElementById("timer").innerText = `⏱ ${seconds} sec`;
+    }
+    document.getElementById("count").innerText = `🔁 Searches: ${data.searchCount || 0} / 100`;
+  });
+}, 1000);
